c97b0126a5200218d48207dbb54f4219a6a185f3
nizoom <nissimram1812@gmail.com>
2025-02-05 15:16:10 -0500

    first commit

---
+++ src/components/top-section/top-section-parent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../css/top-section.css";
+
+const TopSectionParent = () => {
+  return (
+    <section className="top-section-parent">
+      <div className="top-section-banner">
+        <h1>Losing Ground</h1>
+        <h3> SEVERE REPETATIVE FLOODING IN THE UNITED STATES </h3>
+        <h3> WHAT IS THE NFIP ? </h3>
+      </div>
+      <div className="info-panel">
+        <div>
+          <h5> SEVERE REPETATIVE PROPERTY LOSS (SRPL)</h5>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopSectionParent;
